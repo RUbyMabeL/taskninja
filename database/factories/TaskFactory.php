b07797d50f211e8aaa485c1209828b6c7c984444
Xiaona Wan <wan00035@algonquinlive.com>
2023-08-05 16:47:00 -0400

    add a priority table

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -21,7 +21,7 @@ class TaskFactory extends Factory
             'user_id' => $this->faker->numberBetween(1, 10),
             'list_id' => $this->faker->numberBetween(1, 5),
             'due_date' => $this->faker->date(),
-            'priority' => $this->faker->word(),
+            'priority' => $this->faker->randomElement(['low', 'medium', 'high']),
             'completed' => $this->faker->boolean(false)
         ];
     }
